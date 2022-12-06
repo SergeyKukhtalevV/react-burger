2e971f2b2e4202ecaf51d1566e0d8459b76484c2
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-12-06 22:50:51 +0300

    bugfix: removed className for BurgerElement's img

--- src/components/burger-element/burger-element.jsx
+++ src/components/burger-element/burger-element.jsx
@@ -6,8 +6,7 @@ const BurgerElement = (props) => {
   return (
     <li className={`ml-4 mr-2 ${burgerElementStyles.card}`}>
       <Counter count={1} size="default" extraClass="m-1"/>
-      <img className={BurgerElement.image} src={props.image}
-           alt={props.name}/>
+      <img src={props.image} alt={props.name}/>
       <div className={`mt-1 ${burgerElementStyles.price}`}>
         <p className={`text text_type_digits-default`}>{20} </p>
         <CurrencyIcon type="primary"/>
