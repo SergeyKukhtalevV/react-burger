1dd8906123790efafbdf1bb0823432f40340034c
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-12-08 23:30:30 +0300

    feature: api added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,6 @@ import appStyles from './app.module.css';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import data from '../../utils/data';
 import order from "../../utils/order";
 
 const urlApi = 'https://norma.nomoreparties.space/api/ingredients ';
@@ -11,30 +10,33 @@ const urlApi = 'https://norma.nomoreparties.space/api/ingredients ';
 function App() {
 
   const [state, setState] = useState({
-    ingredientsData: null,
+    ingredientsData: [],
     loading: true
-  })
+  });
 
   useEffect(() => {
-    const getIngredientsData = async () => {
-      setState({...state, loading: true});
-      const res = await fetch(urlApi);
-      const data = await res.json();
-      setState({ingredientsData: data.data, loading: false});
-    }
-
     getIngredientsData();
   },[]);
 
-  console.log(state.ingredientsData);
+  const getIngredientsData = async () => {
+    setState({...state, loading: true});
+    const res = await fetch(urlApi);
+    const data = await res.json();
+    setState({ingredientsData: data.data, loading: false});
+  }
+
   return (
     <div className={appStyles.appContent}>
       <AppHeader/>
-      
-      <main className={appStyles.main}>
-        <BurgerIngredients data={data} />
-        <BurgerConstructor data={order} />
-      </main>
+      {
+        state.loading
+        ? <p className="text text_type_main-medium">Идет загрузка...</p>
+      :
+        <main className={appStyles.main}>
+          <BurgerIngredients data={state.ingredientsData} />
+          <BurgerConstructor data={order} />
+        </main>
+      }
     </div>
   );
 }
