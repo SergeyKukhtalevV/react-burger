7db9feb71d37293e0f14f1eb6f4bb5e75710c0b3
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-12-08 16:16:12 +0300

    feature: added api in progress

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import appStyles from './app.module.css';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
@@ -6,10 +6,31 @@ import BurgerConstructor from "../burger-constructor/burger-constructor";
 import data from '../../utils/data';
 import order from "../../utils/order";
 
+const urlApi = 'https://norma.nomoreparties.space/api/ingredients ';
+
 function App() {
+
+  const [state, setState] = useState({
+    ingredientsData: null,
+    loading: true
+  })
+
+  useEffect(() => {
+    const getIngredientsData = async () => {
+      setState({...state, loading: true});
+      const res = await fetch(urlApi);
+      const data = await res.json();
+      setState({ingredientsData: data.data, loading: false});
+    }
+
+    getIngredientsData();
+  },[]);
+
+  console.log(state.ingredientsData);
   return (
     <div className={appStyles.appContent}>
       <AppHeader/>
+      
       <main className={appStyles.main}>
         <BurgerIngredients data={data} />
         <BurgerConstructor data={order} />
