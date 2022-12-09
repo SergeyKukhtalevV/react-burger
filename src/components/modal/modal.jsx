adc2a49c1767255746e0f298c29e27ba3e83863a
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-12-09 21:24:37 +0300

    feature: made order's modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import modalStyles from './modal.module.css'
 import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 const Modal = ({active, setActive, children}) => {
 
   function closeModalByEscape(e) {
-    if (e.nativeEvent.key === 'Escape') {
+    if (e.key === 'Escape') {
       setActive(false);
     }
   }
 
-  return (
+  React.useEffect(() => {
+    document.addEventListener('keydown', closeModalByEscape);
+    return () => document.removeEventListener('keydown', closeModalByEscape);
+  });
+  return ReactDOM.createPortal(
     <div
-      className={active ? [modalStyles.modal__overlay, modalStyles.modal_active].join(' ') : modalStyles.modal__overlay}
-      onClick={() => {setActive(false)}}
+      className={active
+        ? [modalStyles.modal__overlay, modalStyles.modal_active].join(' ')
+        : modalStyles.modal__overlay}
       onKeyDown={(e) => {
         closeModalByEscape(e)
+      }}
+      onClick={() => {
+        setActive(false)
       }}
       tabIndex='-1'
     >
 
       <div className={`${modalStyles.modal__content}`} onClick={e => e.stopPropagation()}>
         <div className={modalStyles.modal__iconClose}>
-          <CloseIcon type="primary" onClick={() => {setActive(false)}}/>
+          <CloseIcon type="primary" onClick={() => {
+            setActive(false)
+          }}/>
         </div>
         {children}
       </div>
-    </div>
+    </div>, document.getElementById('react-modals')
   );
 };
 
