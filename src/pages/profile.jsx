5e0f9a54f022573986da14d86a87cb01783746de
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-03-02 10:14:12 +0300

    feat: profile page done

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -13,30 +13,34 @@ const ProfilePage = () => {
     setValue({...form, [e.target.name]: e.target.value});
   }
   return (
-    <main className={styles.container}>
-      <ul className={styles.menu}>
-        <li>
-          <NavLink to={"/profile"} className={`${styles.link} text text_type_main-medium`}>Профиль</NavLink>
-        </li>
-        <li>
-          <NavLink to={"/profile/orders"} className={`${styles.link} text text_type_main-medium`}>История заказов</NavLink>
-        </li>
-        <li>
-          <NavLink to={"/"} className={`${styles.link} text text_type_main-medium`}>Выход</NavLink>
-        </li>
-        <li className={"mt-20 "}>
-          <p className={"text text_type_main-default text_color_inactive"}>В этом разделе вы можете
-            изменить свои персональные данные</p>
-        </li>
-      </ul>
-      <form className={`${styles.form}`}>
-        <Input type={"text"} extraClass={``} placeholder={'Имя'} value={form.name} name={"name"}
-               onChange={onChange} icon={"EditIcon"}/>
-        <EmailInput extraClass={`mt-6`} placeholder={'E-mail'} value={form.email} name={"email"} onChange={onChange} icon={"EditIcon"}/>
-        <PasswordInput extraClass={`mt-6`} placeholder={'пароль'} value={form.password} name={"password"}
-                       onChange={onChange}
-                       icon={"EditIcon"}/>
-      </form>
+    <main className={styles.wrapper}>
+      <div className={styles.container}>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={"/profile"} className={`${styles.link} text text_type_main-medium`}>Профиль</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/profile/orders"} className={`${styles.link} text text_type_main-medium`}>История
+              заказов</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"} className={`${styles.link} text text_type_main-medium`}>Выход</NavLink>
+          </li>
+          <li className={"mt-20 "}>
+            <p className={"text text_type_main-default text_color_inactive"}>В этом разделе вы можете
+              изменить свои персональные данные</p>
+          </li>
+        </ul>
+        <form className={`${styles.form}`}>
+          <Input type={"text"} extraClass={``} placeholder={'Имя'} value={form.name} name={"name"}
+                 onChange={onChange} icon={"EditIcon"}/>
+          <EmailInput extraClass={`mt-6`} placeholder={'Логин'} value={form.email} name={"email"} onChange={onChange}
+                      icon={"EditIcon"}/>
+          <PasswordInput extraClass={`mt-6`} placeholder={'пароль'} value={form.password} name={"password"}
+                         onChange={onChange}
+                         icon={"EditIcon"}/>
+        </form>
+      </div>
     </main>
   );
 };
