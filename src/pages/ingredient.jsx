37cbad901817c3a8cf8dfe9ccbf1ded97b3120ae
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-03-11 08:43:07 +0300

    feat: fix ingredient-page

--- src/pages/ingredient.jsx
+++ src/pages/ingredient.jsx
@@ -23,43 +23,46 @@ const IngredientPage = ({isActive, setModalActive}) => {
   }
 
   useEffect(
-    (id) => {
+    () => {
       dispatch(getIngredients());
       setIsLoaded(true);
       setModalActive(true);
       setTimeout(init, 500, [id]);
     },
-    [isActive]
+    []
   );
-
 
   return (
     <Modal active={isActive} setActive={setModalActive}>
-      <div className={`${styles.modal_ingredient}`}>
-        <p className="mt-10 ml-10 pt-3 text text_type_main-large">Детали ингредиента</p>
-        <div className={`${styles.modal_ingredientDesc}`}>
-          <img className={`mt-3`} src={currentIngredient.image_large} alt={"изображение ингредиента"}/>
-          <p className="mt-4 text text_type_main-medium">{currentIngredient.name}</p>
-          <ul className={`mt-8 mb-15 ${styles.modal_ingredientProp}`}>
-            <li className={`${styles.modal_ingredientInfo}`}>
-              <p className="text text_type_main-default  text_color_inactive">Калории,ккал</p>
-              <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.calories}</p>
-            </li>
-            <li className={`${styles.modal_ingredientInfo}`}>
-              <p className="text text_type_main-default  text_color_inactive">Белки, г</p>
-              <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.proteins}</p>
-            </li>
-            <li className={`${styles.modal_ingredientInfo}`}>
-              <p className="text text_type_main-default  text_color_inactive">Жиры, г</p>
-              <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.fat}</p>
-            </li>
-            <li className={`${styles.modal_ingredientInfo}`}>
-              <p className="text text_type_main-default  text_color_inactive">Углеводы, г</p>
-              <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.carbohydrates}</p>
-            </li>
-          </ul>
+      {!isLoaded
+        ? <p className="mt-10 ml-10 mb-10 pt-3 text text_type_main-large">Идет загрузка...</p>
+        :
+        <div className={`${styles.modal_ingredient}`}>
+          <p className="mt-10 ml-10 pt-3 text text_type_main-large">Детали ингредиента</p>
+          <div className={`${styles.modal_ingredientDesc}`}>
+            <img className={`mt-3`} src={currentIngredient.image_large} alt={"изображение ингредиента"}/>
+            <p className="mt-4 text text_type_main-medium">{currentIngredient.name}</p>
+            <ul className={`mt-8 mb-15 ${styles.modal_ingredientProp}`}>
+              <li className={`${styles.modal_ingredientInfo}`}>
+                <p className="text text_type_main-default  text_color_inactive">Калории,ккал</p>
+                <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.calories}</p>
+              </li>
+              <li className={`${styles.modal_ingredientInfo}`}>
+                <p className="text text_type_main-default  text_color_inactive">Белки, г</p>
+                <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.proteins}</p>
+              </li>
+              <li className={`${styles.modal_ingredientInfo}`}>
+                <p className="text text_type_main-default  text_color_inactive">Жиры, г</p>
+                <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.fat}</p>
+              </li>
+              <li className={`${styles.modal_ingredientInfo}`}>
+                <p className="text text_type_main-default  text_color_inactive">Углеводы, г</p>
+                <p className=" text text_type_digits-default  text_color_inactive">{currentIngredient.carbohydrates}</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      }
     </Modal>
   );
 };
